50e2dee088910c0177c0020acfc288d18ce315c9
risq <valentin.ledrapier@gmail.com>
2015-04-23 22:11:47 +0200

    Add basic server structure

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('boilerplateApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
