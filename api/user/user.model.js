7709637b95c39fbb21e2949986aad1d7669435ee
risq <valentin.ledrapier@gmail.com>
2015-06-02 09:27:28 +0200

    Add user model properties & seed

--- api/user/user.model.js
+++ api/user/user.model.js
@@ -8,6 +8,15 @@ var authTypes = ['github', 'twitter', 'facebook', 'google'];
 var UserSchema = new Schema({
   name: String,
   email: { type: String, lowercase: true },
+  age: Number,
+  experience: Number,
+  position: String,
+  specialities: String,
+  birthDate: String,
+  address: String,
+  telephone: String,
+  description: String,
+  img: String,
   role: {
     type: String,
     default: 'user'
--- config/seed.js
+++ config/seed.js
@@ -8,40 +8,190 @@
 var Thing = require('../api/thing/thing.model');
 var User = require('../api/user/user.model');
 
-Thing.find({}).remove(function() {
-  Thing.create({
-    name : 'Development Tools',
-    info : 'Integration with popular tools such as Bower, Grunt, Karma, Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, Stylus, Sass, CoffeeScript, and Less.'
-  }, {
-    name : 'Server and Client integration',
-    info : 'Built with a powerful and fun stack: MongoDB, Express, AngularJS, and Node.'
-  }, {
-    name : 'Smart Build System',
-    info : 'Build system ignores `spec` files, allowing you to keep tests alongside code. Automatic injection of scripts and styles into your index.html'
-  },  {
-    name : 'Modular Structure',
-    info : 'Best practice client and server structures allow for more code reusability and maximum scalability'
-  },  {
-    name : 'Optimized Build',
-    info : 'Build process packs up your templates as a single JavaScript payload, minifies your scripts/css/images, and rewrites asset names for caching.'
-  },{
-    name : 'Deployment Ready',
-    info : 'Easily deploy your app to Heroku or Openshift with the heroku and openshift subgenerators'
-  });
-});
-
 User.find({}).remove(function() {
   User.create({
     provider: 'local',
-    name: 'Test User',
-    email: 'test@test.com',
-    password: 'test'
+    name: 'Test User1',
+    email: 'test1@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User2',
+    email: 'test2@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User3',
+    email: 'test3@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User4',
+    email: 'test4@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User5',
+    email: 'test5@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User6',
+    email: 'test6@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User7',
+    email: 'test7@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User8',
+    email: 'test8@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User9',
+    email: 'test9@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User10',
+    email: 'test10@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User11',
+    email: 'test11@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
+  }, {
+    provider: 'local',
+    name: 'Test User12',
+    email: 'test12@test.com',
+    password: 'test',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
   }, {
     provider: 'local',
     role: 'admin',
     name: 'Admin',
     email: 'admin@admin.com',
-    password: 'admin'
+    password: 'admin',
+    age: 28,
+    experience: 3,
+    position: 'Business Analyst',
+    specialities: 'Informatique - Gestion des ressources humaines - Planification - Finance - Marketing',
+    birthDate: '18 Mars 1987',
+    address: '11 Avenue des Tulipes, 74000 Annecy',
+    telephone: '0684503920',
+    description: 'Iamque non umbratis fallaciis res agebatur, sed qua palatium est extra muros, armatis omne circumdedit. ingressusque obscuro iam die, ablatis regiis indumentis Caesarem tunica texit et paludamento communi, eum post haec nihil passurum velut mandato principis iurandi crebritate confirmans et statim inquit exsurge et inopinum carpento privato inpositum ad Histriam duxit prope oppidum Polam, ubi quondam peremptum Constantini filium accepimus Crispum.',
+    img: 'http://www.corporatetraveller.ca/assets/images/profile-placeholder.gif'
   }, function() {
       console.log('finished populating users');
     }
